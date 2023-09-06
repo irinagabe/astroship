0fa283d21519888b8288453a59569d274c6bf91d
Gabe Vigodi <gabe.vigodi@gmail.com>
2023-09-06 14:03:59 +0300

    Initial commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,12 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
+        sans: [
+          "Montserrat",
+          "Inter Variable",
+          "Inter",
+          ...defaultTheme.fontFamily.sans,
+        ],
       },
     },
   },
